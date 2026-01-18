2e64f1ec570b516a5caa1d43fb7a57d7a86cee13
parnoud <pierre.arnoud@hotmail.fr>
2026-01-18 14:31:12 +0100

    correct hosts update and add replace state

--- plugins/modules/manage_host_groups.py
+++ plugins/modules/manage_host_groups.py
@@ -21,10 +21,13 @@ author: "Pierre ARNOUD (@parnoud)"
 options:
     state:
         description: Desired state of the host.
-        choices: ['create', 'delete', 'update', 'duplicate']
+        choices: ['create', 'delete', 'update', 'replace', 'duplicate']
         default: 'create'
         type: str
         required: False
+    hostgroup_id:
+        description: hostgroup id
+        type: int
     name:
         description: Name of the host group.
         required: False
@@ -82,7 +85,7 @@ from ansible_collections.parnoud.centreon.plugins.module_utils.host_group import
     list_all_host_groups,
     add_host_group,
     duplicate_multiple_host_groups,
-    get_host_groups
+    get_host_group
 )
 from ansible_collections.parnoud.centreon.plugins.module_utils.argument_spec import base_argument_spec
 
@@ -99,7 +102,8 @@ def manage_host_groups(module):
         timeout=module.params.get('timeout'),
     )
 
-    host_group_data = {
+    hostgroup_data = {
+        'hostgroup_id': module.params.get('hostgroup_id'),
         'alias': module.params.get('alias'),
         'comment': module.params.get('comment'),
         'geo_coords': module.params.get('geo_coords'),
@@ -108,69 +112,112 @@ def manage_host_groups(module):
         'name': module.params.get('name'),
     }
 
-    host_group_data = {k: v for k, v in host_group_data.items() if v is not None}
+    hostgroup_data = {k: v for k, v in hostgroup_data.items() if v is not None}
+
 
     if module.params['state'] == 'create':
 
-        result = add_host_group(api, host_group_data=host_group_data)
+        result = add_host_group(api, hostgroup_data=hostgroup_data)
         return True, result
 
-    elif module.params['state'] == 'update':
+    elif module.params['state'] == 'replace':
 
-        host_group_id = None
+        hostgroup_id = None
+        current_hostgroup_data = None
+
         if module.params['new_name']:
-            host_group_data['name'] = module.params['new_name']
-        else:
-            host_group_data.pop('name', None)
+            hostgroup_data['name'] = module.params['new_name']
 
-        if module.params['host_id']:
-            host_group_id = module.params['host_id']
+        if module.params['hostgroup_id']:
+            hostgroup_id = module.params['hostgroup_id']
         elif module.params['name']:
             filter_criteria = {}
             filter_criteria['search'] = json.dumps({'name': module.params['name']})
-            hosts = list_all_host_groups(api, params=filter_criteria)
-            if len(hosts) != 1:
+            hosts_group = list_all_host_groups(api, params=filter_criteria)
+            if len(hosts_group) != 1:
                 return False, f"Host group {module.params['name']} multiple or not found for update."
-            host_group_id = hosts[0]['id']
+            hostgroup_id = hosts_group[0]['id']
+        
+        current_hostgroup_data = get_host_group(api, hostgroup_id=hostgroup_id)
 
-        if host_group_id:
-            if update_host_group(api, host_group_id, host_group_data):
-                return True, get_host_groups(api, host_group_id)
+        if hostgroup_id:
+            hostgroup_data['name'] = current_hostgroup_data['name']
+            if update_host_group(api, hostgroup_id, hostgroup_data):
+                return True, get_host_group(api, hostgroup_id=hostgroup_id)
+        else:
+            return False, "Host ID or name must be provided for update operation."
+
+    elif module.params['state'] == 'update':
+
+        hostgroup_id = None
+        current_hostgroup_data = None
+
+        if module.params['new_name']:
+            hostgroup_data['name'] = module.params['new_name']
+
+        if module.params['hostgroup_id']:
+            hostgroup_id = module.params['hostgroup_id']
+        elif module.params['name']:
+            filter_criteria = {}
+            filter_criteria['search'] = json.dumps({'name': module.params['name']})
+            hosts_group = list_all_host_groups(api, params=filter_criteria)
+            if len(hosts_group) != 1:
+                return False, f"Host group {module.params['name']} multiple or not found for update."
+            hostgroup_id = hosts_group[0]['id']
+        
+        current_hostgroup_data = get_host_group(api, hostgroup_id=hostgroup_id)
+
+        current_hostgroup_data["hosts"] = [item["id"] for item in current_hostgroup_data["hosts"]]
+        if hostgroup_data.get('hosts') != []:
+            current_hostgroup_data["hosts"] += list(hostgroup_data['hosts'])
+
+        hostgroup_data.pop('hosts')
+
+        current_hostgroup_data.pop('id', None)
+        current_hostgroup_data.pop('icon', None)
+        current_hostgroup_data.pop('is_activated', None)
+
+        current_hostgroup_data.update(hostgroup_data)
+
+        if hostgroup_id:
+            if update_host_group(api, hostgroup_id, current_hostgroup_data):
+                return True, get_host_group(api, hostgroup_id=hostgroup_id)
         else:
             return False, "Host ID or name must be provided for update operation."
 
     elif module.params['state'] == 'delete':
 
-        host_group_id = None
+        hostgroup_id = None
 
-        if module.params['host_id']:
-            host_group_id = module.params['host_id']
+        if module.params['hostgroup_id']:
+            hostgroup_id = module.params['hostgroup_id']
         elif module.params['name']:
             filter_criteria = {}
             filter_criteria['search'] = json.dumps({'name': module.params['name']})
-            host_groups = list_all_host_groups(api, query_parameters=filter_criteria)
+            hostgroups = list_all_host_groups(api, params=filter_criteria)
 
-            if len(host_groups) == 0:
-                return True, host_groups
-            elif len(host_groups) >= 2:
-                return False, f"Host {module.params['name']} multiple for update. {len(host_groups)}"
-            host_group_id = host_groups[0]['id']
+            if len(hostgroups) == 0:
+                return True, hostgroups
+            elif len(hostgroups) >= 2:
+                return False, f"Host {module.params['name']} multiple for update. {len(hostgroups)}"
+            hostgroup_id = hostgroups[0]['id']
 
-        if host_group_id:
-            if delete_host_group(api, host_group_id):
-                return True, host_groups[0]
+        if hostgroup_id:
+            if delete_host_group(api, hostgroup_id=hostgroup_id):
+                return True, hostgroups[0]
         else:
             return False, "Host ID or name must be provided for update operation."
 
     elif module.params['state'] == 'duplicate':
-        result = duplicate_multiple_host_groups(api, module.params.get('ids'), module.params.get('nb_duplicates'))
+        result = duplicate_multiple_host_groups(api, ids=module.params.get('ids'), nb_duplicates=module.params.get('nb_duplicates'))
         return True, result
 
 
 def main():
     argument_spec = base_argument_spec()
     argument_spec.update(
-        state=dict(type='str', choices=['create', 'delete', 'update', 'duplicate'], default='create'),
+        state=dict(type='str', choices=['create', 'delete', 'update', 'replace', 'duplicate'], default='create'),
+        hostgroup_id=dict(type='int'),
         name=dict(type='str'),
         new_name=dict(type='str'),
         alias=dict(type='str'),
